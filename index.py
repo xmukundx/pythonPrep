cb30b456fa6b3df248914634329d181660435381
xmukundx <144305233+xmukundx@users.noreply.github.com>
2024-09-16 15:25:11 +0530

    Create index.py
    Recursion

---
+++ index.py
@@ -0,0 +1,52 @@
+# TOPIC - RECURSION
+
+
+fac = 1
+# for i in range(1, 5+1):
+#     fac *= i
+#     print(fac)
+i = 5
+# while 0<+i:
+#     fac *= i
+#     i-=1
+# print(fac)
+
+def factorial (par):
+    if (par == 0 or par == 1):
+        return 1
+    else:
+        return par * factorial(par-1)
+    
+    
+# print(factorial(5))
+
+
+# def sum(num):
+#   if num == 0 or num == 1:
+#     return 1
+  
+#   else:
+#     return num + sum(num-1)
+# ans = sum(5);
+
+# print(ans)
+
+
+
+li = [1,2,6,88,4545,12]
+
+for i in range(li[i]):
+    print(li[i])
+
+    
+    
+li = [1, 2, 6, 88, 4545, 12]
+
+for i in range(len(li)):
+    print(li[i])
+    
+    
+    
+    
+    
+    
